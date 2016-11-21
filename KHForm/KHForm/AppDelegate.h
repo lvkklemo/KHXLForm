6cde073006c0440d6ced6a134febba94dc4a0b21
lkk2016 <631813042@qq.com>
2016-11-21 10:18:32 +0800

    1.init

---
+++ KHForm/KHForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KHForm
+//
+//  Created by 宇航 on 16/11/21.
+//  Copyright © 2016年 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
